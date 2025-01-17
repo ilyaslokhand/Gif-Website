40dae8e134f6ba9ad5415148b3e74a6f135edf82
ilyas <ilokhandwala786@gmail.com>
2025-01-17 12:00:44 +0530

    done final project

--- src/Pages/Favorites.jsx
+++ src/Pages/Favorites.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { GifState } from "../context/Gif-context";
+import Gif from "../components/Gif";
 
 const Favorites = () => {
-  return <div>ok</div>;
+  const [favoritesgif, setfavoritesgif] = useState([]);
+  console.log(favoritesgif);
+  const { Gf, favorites } = GifState();
+
+  const fetchfavoritegif = async () => {
+    const { data: gifs } = await Gf.gifs(favorites);
+    setfavoritesgif(gifs);
+  };
+
+  useEffect(() => {
+    fetchfavoritegif();
+  }, []);
+
+  return (
+    <div className="mt-2">
+      <span className="faded-text ">My Favorites</span>
+
+      <div className="columns-2 md:columns-3 lg:columns-4 xl:columns-5 gap-2 mt-2">
+        {favoritesgif.map((gif) => (
+          <Gif gif={gif} key={gif.id} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Favorites;
